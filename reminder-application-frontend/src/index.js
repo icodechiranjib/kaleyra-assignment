bfde0524caac39a671c1f27f96a333e7218df850
Chiranjib Bordoloi <chiranjib.bordoloi30@gmail.com>
2022-08-20 07:11:37 +0530

    routes used

--- reminder-application-frontend/src/index.js
+++ reminder-application-frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
   <div>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
   </div>,
   document.getElementById("root")
 );
