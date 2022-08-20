07b347478a413917bbf8e72f0f0ada9016291a0e
Chiranjib Bordoloi <chiranjib.bordoloi30@gmail.com>
2022-08-20 07:19:54 +0530

    changed server.js

--- server.js
+++ server.js
@@ -159,6 +159,7 @@ app.post("/deleteReminder", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
