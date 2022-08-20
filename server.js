0be6f32af1fbc8652b37e1ccfbbc0bb649b9c812
Chiranjib Bordoloi <chiranjib.bordoloi30@gmail.com>
2022-08-20 07:29:26 +0530

    server -> folder-name

--- server.js
+++ server.js
@@ -158,10 +158,17 @@ app.post("/deleteReminder", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("reminder-application-frontend/build"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        "reminder-application-frontend",
+        "build",
+        "index.html"
+      )
+    );
   });
 }
 
